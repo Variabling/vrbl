fc9fa76bad78f39afd0a077382ba8d4e49bd934d
Tpro Oficial <77292734+tpro99oficial@users.noreply.github.com>
2021-12-14 18:11:33 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+a = "Welcome to Variabling"
+print(a)
+f = "Variabling is a Free Open-Source Code Designed to Compile Variables in a Single File in Diverse Languages"
+print(f)
+g = "Variabling is avaible on github"
+l = "Variabling in GitHub is: github.com/Variabling"
+print(g)
+print(l)
+r = "Everything you see in this Console is writed using variables called a,f,g,l,r "
+print(r)
+e = "to contribute to variabling go to github.com/variable/vrbl"
+print(e)
+1 = "to c"
